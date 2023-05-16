883d56c3ea3ecd918e048726a680c7ccfbd2440c
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-05-17 01:01:32 +0530

    commit

--- javascript.js
+++ javascript.js
@@ -21,12 +21,11 @@ const quizData = [
         correct: "b",
     },
     {
-        question: "?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
+        question: "How to fetch element using its Id?",
+        a: "getElementByClass",
+        b: "getElementByTag",
+        c: "getElementById",
+        correct: "c",
     },
 ];
 const quiz= document.getElementById('quiz')
@@ -35,7 +34,6 @@ const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
 let currentQuiz = 0
 let score = 0
@@ -47,7 +45,6 @@ function loadQuiz() {
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
 }
 function deselectAnswers() {
     answerEls.forEach(answerEl => answerEl.checked = false)
